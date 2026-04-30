646727bd654b5a69ceebfab29c5a86644132d185
Bene72 <benoit.buon.lms@gmail.com>
2026-04-30 15:31:34 +0200

    Update SectionHead.js

--- components/ui/SectionHead.js
+++ components/ui/SectionHead.js
@@ -1,28 +1,24 @@
 export default function SectionHead({ title, caption, action }) {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'flex-start',
-        gap: 12,
-        marginBottom: 14,
-      }}
-    >
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'space-between', 
+      alignItems: 'flex-start', 
+      gap: 10, 
+      marginBottom: 10 
+    }}>
       <div>
-        <div
-          style={{
-            fontWeight: 900,
-            color: '#0D1B4E',
-            fontSize: 15,
-            lineHeight: 1.2,
-            marginBottom: caption ? 6 : 0,
-          }}
-        >
+        <div style={{ 
+          fontWeight: 900, 
+          color: '#0D1B4E', 
+          fontSize: 13, 
+          lineHeight: 1.2, 
+          marginBottom: caption ? 4 : 0 
+        }}>
           {title}
         </div>
         {caption ? (
-          <div style={{ color: '#6B7A99', fontSize: 13, lineHeight: 1.55 }}>{caption}</div>
+          <div style={{ color: '#6B7A99', fontSize: 11, lineHeight: 1.5 }}>{caption}</div>
         ) : null}
       </div>
       {action ? <div>{action}</div> : null}
