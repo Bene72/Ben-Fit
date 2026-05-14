66c92760dc5ec22cddf94d77d0447cf3625be191
Bene72 <benoit.buon.lms@gmail.com>
2026-05-14 19:50:58 +0200

    Create normalizeExerciseName.js

---
+++ components/programme/utils/normalizeExerciseName.js
@@ -0,0 +1,9 @@
+export function normalizeExerciseName(name) {
+  return name
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[̀-ͯ]/g, '')
+    .replace(/[^a-z0-9 ]/g, ' ')
+    .replace(/\s+/g, ' ')
+    .trim()
+}
