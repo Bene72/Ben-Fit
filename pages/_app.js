618a0c875ceb7fbf5c152da1da5017671227a679
Bene72 <benoit.buon.lms@gmail.com>
2026-08-31 08:42:38 +0200

    Remove unused CSS import in _app.js
    Remove import of ui-foundation.css from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '../styles/globals.css'
-import '../styles/ui-foundation.css'
 import ErrorBoundary from '../components/ErrorBoundary'
 
 export default function App({ Component, pageProps }) {
