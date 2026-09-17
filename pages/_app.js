10a381650fc5aa087d24a7b35ca56abe5c62ce0c
Bene72 <benoit.buon.lms@gmail.com>
2026-09-17 07:16:48 +0200

    Import tokens.css for global styling
    Added import for global tokens CSS file.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+// Thème pastel de l'espace athlète, scopé à `.athlete-theme` (posée par
+// AppShell). Ne touche pas l'espace coach : voir styles/tokens.css.
+import '../styles/tokens.css'
 import ErrorBoundary from '../components/ErrorBoundary'
 
 export default function App({ Component, pageProps }) {
